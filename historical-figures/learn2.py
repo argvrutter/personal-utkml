3dabbb9ba21f841804fb18c7639642ac4b4ecc21
Aiden <solsane@gmail.com>
2018-06-06 13:48:46 -0400

    added dir

---
+++ historical-figures/learn2.py
@@ -0,0 +1,4 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
